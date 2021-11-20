0171e5418049a95b2d1342ea078dcaa19c8412f8
Bekhzod Akbarov <stx.webdev@gmail.com>
2021-11-20 12:08:57 +0500

    create edit & create routes

--- app/router.js
+++ app/router.js
@@ -8,4 +8,6 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('post', { path: '/posts/:id' });
+  this.route('create', { path: '/posts/new' });
+  this.route('edit', { path: '/posts/:id/edit' });
 });
